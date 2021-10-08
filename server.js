989153a0cd170126f5e457bd0c52c1fe4ea075cd
kandagatla9 <srinathkandagatla6@gmail.com>
2021-10-08 18:48:59 +0530

    update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CRUD'));
+app.use(express.static(__dirname + '/dist/crud'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/CRUD/index.html'));
+res.sendFile(path.join(__dirname+'/dist/crud/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
